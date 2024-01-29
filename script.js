17a3539a8de6a045fc93cd6d486c6dda2457ba65
Iure Finotti <iure@sysout.com.br>
2024-01-29 16:36:51 -0300

    Aula 4 - Pt2

--- script.js
+++ script.js
@@ -1,17 +1,45 @@
+// Referencias aos elementos HTML
+
 const searchInput = document.getElementById('search-input');
-const resultArtist = document.getElementById('result-artist')
-const resultPlaylist = document.getElementById('result-playlists')
+const resultArtist = document.getElementById('result-artist');
+const resultPlaylist = document.getElementById('result-playlists');
+
+// Função para fazer solicitacao a api quando se pesquisa algo na aplicação
 
 function requestApi(searchTerm) {
-    const url = "http://localhost:3000/artists?name_like=$(searchTerm)"
-    fetch(url)
+
+    const search = searchTerm; // Your search term
+
+    fetch('http://localhost:3000/artists')
         .then((response) => response.json())
-        .then((result) => displayResults(result))
+        .then(users => {
+        
+            // Filter users whose names contain the search term
+            const matchedUsers = users.filter(user =>
+              user.name.toLowerCase().includes(search.toLowerCase())
+            );
+        
+            console.log(matchedUsers);
+          })
+        // .then((result) => displayResults(result))
+}
+
+// Função para mostrar o nome e imagem dos artistas quando são buscados
+function displayResults(result) {
+    resultPlaylist.classList.add('hidden');
+    const artistName = document.getElementById('artist-name');
+    const artistImage = document.getElementById('artist-img');
+
+    result.forEach(element => {
+        artistName.innerText = element.name;
+        artistImage.src = element.urlImg;
+    });
+
+    resultArtist.classList.remove('hidden');
 }
 
 document.addEventListener('input', function() {
     const searchTerm = searchInput.value.toLowerCase(); 
-        //Se não digitei nada, todos os cards vão aparecer
         if (searchTerm === '') {
             resultPlaylist.classList.add('hidden');
             resultArtist.classList.remove('hidden');
@@ -19,4 +47,4 @@ document.addEventListener('input', function() {
         }
 
         requestApi(searchTerm);
-})
+}) 
